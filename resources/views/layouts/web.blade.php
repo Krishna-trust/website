f828de35912863f3177cc08052e3236e921bf869
Dhruv Patel <dhruv@selcom.net>
2025-03-06 23:32:32 +0530

    implement google tag maanger

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -7,6 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Krishna Niswarth Seva Trust')</title> <!-- Default title can be overridden -->
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WJRKNRWH');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <meta name="description" content="ક્રિષ્ના નિ:સ્વાર્થ સેવા ટ્રસ્ટ એકલા રહેતાં અશક્ત વડીલોને ઘરે બેઠાં વિનામૂલ્યે ભોજન પહોંચાડતી સંસ્થા.">
 
     <!-- Keywords Tag (Gujarati and English) -->
@@ -27,50 +46,50 @@
 
     <!-- Structured Data for Local Business in Gujarati and English -->
     <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      "name": "Krishna Niswarth Seva Trust",
-      "logo": "{{ asset('images/logo.png') }}",
-      "telephone": "+919898445831",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "જૂની શિવમ ગેસ એજન્સીની સામે | Opp Old Shivam Gas Agency",
-        "addressLocality": "ચાંદલોડિયા, અમદાવાદ, | Chandlodiya, Ahmedabad",
-        "addressRegion": "ગુજરાત | Gujarat",
-        "postalCode": "382481",
-        "addressCountry": "IN"
-      },
-      "sameAs": [
-        "https://chat.whatsapp.com/IKoFgfff0o64XjKEtutY4P",
-        "https://www.facebook.com/people/Krishna-Niswarth-Seva-Trust/pfbid02JJW4F82dQP3szekEKW7R3cfHeGLG8jAK8USN19vivRu8dVQJUVwBmzfUZz6Y7FMyl/",
-        "https://www.youtube.com/@krishnaniswarthsevatrust",
-        "https://www.instagram.com/krishnaniswarth/?utm_source=qr&igsh=OGpwNnp5YWFqaHg0",
-      ],
-      "url": "https://www.krishnaniswarthsevatrust.com/"
-    }
+        {
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "name": "Krishna Niswarth Seva Trust",
+            "logo": "{{ asset('images/logo.png') }}",
+            "telephone": "+919898445831",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "જૂની શિવમ ગેસ એજન્સીની સામે | Opp Old Shivam Gas Agency",
+                "addressLocality": "ચાંદલોડિયા, અમદાવાદ, | Chandlodiya, Ahmedabad",
+                "addressRegion": "ગુજરાત | Gujarat",
+                "postalCode": "382481",
+                "addressCountry": "IN"
+            },
+            "sameAs": [
+                "https://chat.whatsapp.com/IKoFgfff0o64XjKEtutY4P",
+                "https://www.facebook.com/people/Krishna-Niswarth-Seva-Trust/pfbid02JJW4F82dQP3szekEKW7R3cfHeGLG8jAK8USN19vivRu8dVQJUVwBmzfUZz6Y7FMyl/",
+                "https://www.youtube.com/@krishnaniswarthsevatrust",
+                "https://www.instagram.com/krishnaniswarth/?utm_source=qr&igsh=OGpwNnp5YWFqaHg0",
+            ],
+            "url": "https://www.krishnaniswarthsevatrust.com/"
+        }
     </script>
 
     <!-- Structured Data for Reviews (In Gujarati and English) -->
     <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "Review",
-      "itemReviewed": {
-        "@type": "Product",
-        "name": "Krishna Niswarth Seva Trust"
-      },
-      "reviewRating": {
-        "@type": "Rating",
-        "bestRating": "5",
-        "ratingValue": "4.8",
-        "worstRating": "1"
-      },
-      "author": {
-        "@type": "Person",
-        "name": "ચંદ્રકાંતભાઈ પટેલ | Chandrakantbhai Patel"
-      }
-    }
+        {
+            "@context": "https://schema.org",
+            "@type": "Review",
+            "itemReviewed": {
+                "@type": "Product",
+                "name": "Krishna Niswarth Seva Trust"
+            },
+            "reviewRating": {
+                "@type": "Rating",
+                "bestRating": "5",
+                "ratingValue": "4.8",
+                "worstRating": "1"
+            },
+            "author": {
+                "@type": "Person",
+                "name": "ચંદ્રકાંતભાઈ પટેલ | Chandrakantbhai Patel"
+            }
+        }
     </script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -79,12 +98,17 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-      <!-- Hreflang Tags for Multilingual Content -->
-      <link rel="alternate" href="https://www.krishnaniswarthsevatrust.com/" hreflang="en" />
+    <!-- Hreflang Tags for Multilingual Content -->
+    <link rel="alternate" href="https://www.krishnaniswarthsevatrust.com/" hreflang="en" />
     <link rel="alternate" href="https://www.krishnaniswarthsevatrust.com/gu" hreflang="gu" />
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJRKNRWH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <!-- Include the header -->
     @include('web.parts.header')
 
